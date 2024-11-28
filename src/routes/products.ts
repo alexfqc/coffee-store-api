a9415ed382232c5004a8635d74d4402f6024479a
Alessander Franca <alessander70@gmail.com>
2024-11-28 17:25:56 -0300

    [N/A] get product by id added and delete by id fixed

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,6 +1,6 @@
 import fastifyPlugin from "fastify-plugin";
 import { productsSchema } from "../schemas/products.ts";
-
+import { ObjectId } from "mongodb";
 /**
  * Encapsulates the routes
  * @param {FastifyInstance} fastify  Encapsulated Fastify Instance
@@ -16,7 +16,17 @@ async function routes(fastify): Promise<void> {
       const result = await collection.find().toArray();
       return result;
     } catch (e) {
-      return e.message;
+      throw new Error(e);
+    }
+  });
+
+  fastify.get("/products/:id", async (request) => {
+    try {
+      const { id } = request.params;
+      const result = await collection.findOne({ _id: new ObjectId(id) });
+      return result;
+    } catch (e) {
+      throw new Error(e);
     }
   });
 
@@ -28,10 +38,28 @@ async function routes(fastify): Promise<void> {
       }
     },
     async (request) => {
-      const result = await collection.insertOne(request.body);
-      return result.insertedId;
+      try {
+        const result = await collection.insertOne(request.body);
+        return result.insertedId;
+      } catch (e) {
+        throw new Error(e);
+      }
     }
   );
+
+  fastify.delete("/products/:id", async (request, reply) => {
+    try {
+      const { id } = request.params;
+      const result = await collection.deleteOne({ _id: new ObjectId(id) });
+
+      if (result.deletedCount <= 0) {
+        return reply.status(404).send(new Error("Product doesn't exist"));
+      }
+      return "ok";
+    } catch (e) {
+      return reply.status(404).send(new Error("Product doesn't exist"));
+    }
+  });
 }
 
 export default fastifyPlugin(routes);
--- src/tests/products.test.ts
+++ src/tests/products.test.ts
@@ -1,5 +1,5 @@
 import { test } from "node:test";
-import { equal, deepEqual } from "node:assert/strict";
+import { equal, deepEqual, ok } from "node:assert/strict";
 import { build } from "../../app.ts";
 import * as Interfaces from "../../src/interfaces/app.ts";
 
@@ -7,6 +7,7 @@ test("products", async (t) => {
   const optsTest: Interfaces.Opts = { logger: false };
   const app = await build(optsTest);
   const productToBeInserted = { name: "test", quantity: 9, price: 5 };
+  let addedID = "";
 
   t.before(async () => {
     await app.mongo.db?.collection("products").drop();
@@ -45,6 +46,15 @@ test("products", async (t) => {
         payload: productToBeInserted
       });
       equal(res.statusCode, 200);
+
+      const getRes = await app.inject({
+        method: "GET",
+        url: `products/${addedID}`
+      });
+
+      const product = JSON.parse(getRes.payload);
+
+      ok(res.body, product._id);
     }
   );
 
@@ -56,10 +66,46 @@ test("products", async (t) => {
 
     const productsList = JSON.parse(res.payload);
     const product = productsList[0];
+    addedID = product._id;
 
     equal(productsList.length, 1);
     equal(product.name, productToBeInserted.name);
     equal(product.price, productToBeInserted.price);
     equal(product.quantity, productToBeInserted.quantity);
+  });
+
+  await t.test("should get one product", async () => {
+    const res = await app.inject({
+      method: "GET",
+      url: `products/${addedID}`
+    });
+
+    const product = JSON.parse(res.payload);
+
+    equal(res.statusCode, 200);
+    equal(product.name, productToBeInserted.name);
+    equal(product.price, productToBeInserted.price);
+    equal(product.quantity, productToBeInserted.quantity);
+  });
+
+  await t.test(
+    "should return an error if user tries to remove a product that does not exist",
+    async () => {
+      const res = await app.inject({
+        method: "DELETE",
+        url: "products/1"
+      });
+
+      equal(res.statusCode, 404);
+    }
+  );
+
+  await t.test("should return ok if product was removed properly", async () => {
+    const res = await app.inject({
+      method: "DELETE",
+      url: `products/${addedID}`
+    });
+
+    equal(res.payload, "ok");
   });
 });
